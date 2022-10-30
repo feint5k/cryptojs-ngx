c99fb0e3c5208c9d00c56bbc4cec542c5cb29d4e
feint5k <feint5k@proton.me>
2022-10-30 19:56:19 +0000

    appveyor fix

---
+++ src/app/dashboard/coin-of-the-day/coin-of-the-day.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CoinOfTheDayService } from './coin-of-the-day.service';
+import { ref, onValue, Database } from '@angular/fire/database';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+    selector: 'app-coin-of-the-day',
+    templateUrl: './coin-of-the-day.component.html',
+    styleUrls: ['./coin-of-the-day.component.css'],
+    standalone: true,
+    imports: [MatCardModule]
+})
+export class CoinOfTheDayComponent implements OnInit {
+  coinOfTheDay?: { name: string; symbol: string } = undefined;
+
+  constructor(
+    private coinOfTheDayService: CoinOfTheDayService,
+    private database: Database
+  ) {}
+
+  ngOnInit(): void {
+    this.coinOfTheDay = this.coinOfTheDayService.coin;
+
+    if (!this.coinOfTheDay) {
+      this.coinOfTheDayService.fetchCoinOfTheDay().subscribe(() => {
+        this.coinOfTheDay = this.coinOfTheDayService.coin;
+      });
+    }
+
+    const databaseRef = ref(this.database, 'users');
+    onValue(databaseRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+  }
+}
---
+++ src/app/dashboard/crypto-prices/crypto-prices.component.spec.ts
@@ -0,0 +1,7 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CryptoPricesComponent } from './crypto-prices.component';
+
+describe('CryptoPricesComponent', () => {
+  let component: CryptoPricesComponent;
+  let fixture: Compon
