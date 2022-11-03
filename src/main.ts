1c2754e68a7da6cf15d66602aeee3c25098a5f2b
feint5k <feint5k@proton.me>
2022-11-03 19:43:15 +0000

    Fixed issue.

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,64 @@ import {
 import { providePerformance, getPerformance } from '@angular/fire/performance';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideFunctions, getFunctions } from '@angular/fire/functions';
-import { provide
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { AppRoutingModule } from './app/app-routing.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  withInterceptorsFromDi,
+  provideHttpClient,
+} from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import {
+  ScreenTrackingService,
+  UserTrackingService,
+  provideAnalytics,
+  getAnalytics,
+} from '@angular/fire/analytics';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule,
+      MatCardModule,
+      MatSnackBarModule,
+      AppRoutingModule,
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAnalytics(() => getAnalytics()),
+      provideAuth(() => getAuth()),
+      provideDatabase(() => getDatabase()),
+      provideFirestore(() => getFirestore()),
+      provideFunctions(() => getFunctions()),
+      provideMessaging(() => getMessaging()),
+      providePerformance(() => getPerformance()),
+      provideRemoteConfig(() => getRemoteConfig()),
+      provideStorage(() => getStorage()),
+      FirebaseModule
+    ),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err));
